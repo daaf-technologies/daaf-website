8e281d3672b1b9a35834ff6e398c34ea3f4a6717
roslind-vasantha <roslindvasantha@gmail.com>
2025-08-13 00:07:19 +0530

    refactor(#14): :recycle: Added a documentation for button component

--- src/lib/components/design/button/button.svelte
+++ src/lib/components/design/button/button.svelte
@@ -37,3 +37,55 @@
 		{@render children()}
 	</SecondaryButton>
 {/if}
+
+<!-- @component
+### Takes in these props
+
+```ts
+interface ButtonPropsType {
+    children: Snippet;
+    onclick?: () => void;
+    type?: 'submit' | 'button';
+    variant: 'primary' | 'secondary';
+    loading?: boolean;
+    disabled?: boolean;
+    left?: Snippet;
+    right?: Snippet;
+    class?: string;
+}
+
+children: Snippet – content inside the button
+
+onclick: function – click handler
+
+type: 'submit' | 'button' – defaults to 'button'
+
+variant: 'primary' | 'secondary' – defines which button style to render
+
+loading: boolean – shows loading state if supported by inner button
+
+disabled: boolean – disables the button
+
+left: Snippet – icon or element on the left of the label
+
+right: Snippet – icon or element on the right of the label
+
+class: string – apply custom styles to the button
+
+Example usage with left/right:
+
+{#snippet leftIcon(color: string)}
+    <span>
+        <Add />
+    </span>
+{/snippet}
+
+{#snippet rightIcon(color: string)}
+    <span>
+        <ArrowRight />
+    </span>
+{/snippet}
+
+<Button variant="primary" left={leftIcon}>Save</Button>
+<Button variant="secondary" right={rightIcon}>Next</Button>
+-->
