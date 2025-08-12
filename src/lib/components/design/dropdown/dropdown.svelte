4a975a00b52ecffc4e6e4927f110d824fda8ecb0
roslind-vasantha <roslindvasantha@gmail.com>
2025-08-12 23:33:53 +0530

    refactor(#15): :recycle: Made the dropdown component into one

--- src/lib/components/design/dropdown/dropdown.svelte
+++ src/lib/components/design/dropdown/dropdown.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import { type Component } from 'svelte';
-	import DropdownAlternate from './dropdown-alternate.svelte';
-	import DropdownBasic from './dropdown-basic.svelte';
+	import ChevronDown from '$lib/assets/icons/chevron-down.svelte';
+	import { onMount, type Component } from 'svelte';
 
 	interface DropdownPropsType {
 		options: {
@@ -14,11 +13,121 @@
 		icon?: Component;
 	}
 
-	let { options, selectLanguage, value, icon }: DropdownPropsType = $props();
+	let { options, selectLanguage, value, icon: LeftIcon }: DropdownPropsType = $props();
+
+	let open = $state(false);
+
+	const toggle = () => (open = !open);
+
+	const handleClickOutside = (e: MouseEvent) => {
+		if (!(e.target as HTMLElement).closest('.dropdown')) open = false;
+	};
+
+	onMount(() => {
+		document.addEventListener('click', handleClickOutside);
+		return () => document.removeEventListener('click', handleClickOutside);
+	});
 </script>
 
-{#if icon}
-	<DropdownAlternate {options} {selectLanguage} {value} {icon} />
-{:else}
-	<DropdownBasic {options} {selectLanguage} {value} />
-{/if}
+<div class="dropdown">
+	<button class="dropdown-menu" onclick={toggle}>
+		<span>
+			<LeftIcon />
+		</span>
+		{value}
+		<span class:icon-open={open} class:icon-close={!open}>
+			<ChevronDown />
+		</span>
+	</button>
+	<div class="dropdown-options" class:open>
+		{#each options as { label, value: selectedValue, icon: Icon }}
+			<button
+				class="dropdown-item"
+				class:selectedItem={value === selectedValue}
+				role="menuitem"
+				onclick={() => {
+					selectLanguage(selectedValue);
+					value = selectedValue;
+					open = false;
+				}}
+			>
+				<Icon />
+				{label}
+			</button>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.dropdown {
+		position: relative;
+		width: 120px;
+	}
+
+	.dropdown-menu {
+		background-color: #21231e;
+		color: #ffffff;
+		border-radius: 8px;
+		padding: 6px 12px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 10px;
+		cursor: pointer;
+	}
+
+	.dropdown-options {
+		width: 176px;
+		position: absolute;
+		left: 0;
+		top: calc(100% + 8px);
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		background-color: #21231e;
+		color: #ffffff;
+		padding: 8px;
+		border: 2px solid #696969;
+		border-radius: 20px;
+		opacity: 0;
+		pointer-events: none;
+		transform: translateY(-10px);
+		transition:
+			opacity 150ms ease-in-out,
+			transform 150ms ease-in-out;
+	}
+
+	.open {
+		opacity: 1;
+		transform: translateY(0);
+		pointer-events: auto;
+	}
+
+	.dropdown-item {
+		width: 100%;
+		padding: 16px;
+		border-radius: 14px;
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+		gap: 8px;
+	}
+
+	.dropdown-item:hover {
+		background-color: #323332;
+	}
+
+	.selectedItem {
+		background-color: #323332;
+	}
+
+	.icon-open {
+		transform: rotate(180deg);
+		transition: transform 150ms ease;
+	}
+
+	.icon-close {
+		transform: rotate(0deg);
+		transition: transform 150ms ease;
+	}
+</style>
