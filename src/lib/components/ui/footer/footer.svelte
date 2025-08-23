5feca7bee948144da05c85676cc81be6f7f908ea
roslind-vasantha <roslindvasantha@gmail.com>
2025-08-24 01:51:46 +0530

    feat(#10): :sparkles: Added mobile view

--- src/lib/components/ui/footer/footer.svelte
+++ src/lib/components/ui/footer/footer.svelte
@@ -1,32 +1,73 @@
 <script lang="ts">
-	import { DAFF, Facebook, LinkedIn, X, Instagram } from '$lib/assets/icons';
+	import { DAFF, Facebook, LinkedIn, X, Instagram, UK, Dubai, Globe } from '$lib/assets/icons';
 	import Copyright from '$lib/assets/icons/copyright.svelte';
+	import Dropdown from '$lib/components/design/dropdown/dropdown.svelte';
+
+	let options = [
+		{
+			icon: UK,
+			label: 'English',
+			value: 'English'
+		},
+		{
+			icon: Dubai,
+			label: 'Arabic',
+			value: 'Arabic'
+		}
+	];
+	let value = $state('English');
+
+	function selectLanguage(lang: string) {
+		value = lang;
+	}
 </script>
+
+{#snippet left()}
+	<span>
+		<Globe fill="#E6E6E6" />
+	</span>
+{/snippet}
 
 <div class="footer-container">
 	<div class="footer-top">
 		<div class="top-left">
 			<DAFF />
-			<p class="top-left-title">Digital Augmentation & Application Factory</p>
+			<p class="top-left-title">
+				<span class="first-letter"> D </span>igital
+				<span class="first-letter"> A </span>ugmentation &
+				<span class="first-letter"> A </span>pplication
+				<span class="first-letter"> F </span>actory
+			</p>
 		</div>
 		<div class="top-right">
-			<div class="top-right-list">
-				<p class="top-right-title">MENU</p>
-				<p class="top-right-content">Home</p>
-				<p class="top-right-content">About us</p>
-				<p class="top-right-content">Contact us</p>
+			<div class="top-right-first">
+				<div class="top-right-list">
+					<p class="top-right-title">MENU</p>
+					<p class="top-right-content">Home</p>
+					<p class="top-right-content">About us</p>
+					<p class="top-right-content">Contact us</p>
+				</div>
+
+				<div class="top-right-list">
+					<p class="top-right-title">COMPLIANCES</p>
+					<p class="top-right-content">Privacy Policy</p>
+					<p class="top-right-content">Terms & Conditions</p>
+				</div>
 			</div>
 
-			<div class="top-right-list">
-				<p class="top-right-title">COMPLIANCES</p>
-				<p class="top-right-content">Privacy Policy</p>
-				<p class="top-right-content">Terms & Conditions</p>
-			</div>
+			<div class="top-right-second">
+				<div class="top-right-list">
+					<p class="top-right-title">LANGUAGE</p>
 
-			<div class="top-right-list">
-				<p class="top-right-title">LANGUAGE</p>
-				<!-- placeholder for dropdown -->
-				<p>English</p>
+					<Dropdown
+						{left}
+						variant="secondary"
+						{options}
+						{value}
+						{selectLanguage}
+						dropdownControlClassname="w-full !justify-between text-sm"
+					/>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -53,81 +94,20 @@
 </div>
 
 <style>
-	.footer-container {
-		color: #c4c8cc;
-		display: flex;
-		flex-direction: column;
-	}
-
-	.footer-top {
-		width: 100%;
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.top-left {
-		display: flex;
-		flex-direction: column;
-		gap: 20px;
-	}
-
-	.top-left-title {
-		font-size: 26px;
-		width: 270px;
-	}
-
-	.top-right {
-		display: flex;
-		gap: 80px;
-	}
-
-	.top-right-list {
-		display: flex;
-		flex-direction: column;
-		gap: 20px;
+	.first-letter {
+		font-family: 'Departure Mono';
+		color: #40bb4a;
 	}
 
 	.top-right-title {
+		font-family: 'Departure Mono';
 		color: #76797e;
 		font-size: 14px;
 	}
 
-	.top-right-content {
-		color: #c5c6c9;
-		font-size: 16px;
-	}
-
 	.footer-bottom {
-		width: 100%;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
 		border-top: 1px solid #2d373e;
 		border-bottom: 1px solid #2d373e;
-		padding: 22px 0;
-		margin: 40px 0 22px;
-	}
-
-	.bottom-left {
-		display: flex;
-		flex-direction: column;
-		gap: 12px;
-		width: 270px;
-	}
-
-	.bottom-right {
-		display: flex;
-		gap: 36px;
-	}
-
-	.bottom-left-title {
-		color: #ffffff;
-		font-size: 20px;
-	}
-
-	.bottom-left-content {
-		color: #c4c8cc;
-		font-size: 14px;
 	}
 
 	.copyright {
@@ -135,5 +115,154 @@
 		gap: 4px;
 		justify-content: center;
 		align-items: center;
+	}
+	@media (max-width: 768px) {
+		.footer-container {
+			padding: 0 16px;
+			background-color: #03100e;
+			color: #c4c8cc;
+		}
+
+		.top-left {
+			display: flex;
+			flex-direction: column;
+			gap: 16px;
+			padding: 60px 0;
+		}
+
+		.first-letter {
+			font-size: 16px;
+		}
+
+		.top-right {
+			display: flex;
+			flex-direction: column;
+			gap: 40px;
+			padding-bottom: 60px;
+		}
+
+		.top-right-first {
+			display: flex;
+			justify-content: space-between;
+			margin-right: 20px;
+		}
+
+		.top-right-content {
+			color: #c5c6c9;
+			font-size: 20px;
+		}
+
+		.top-right-list {
+			display: flex;
+			flex-direction: column;
+			gap: 20px;
+		}
+
+		.top-right-second {
+			width: 100%;
+		}
+
+		.bottom-right {
+			display: flex;
+			gap: 40px;
+		}
+
+		.footer-bottom {
+			padding: 22px 0;
+			display: flex;
+			flex-direction: column;
+			gap: 30px;
+		}
+
+		.bottom-left-title {
+			font-size: 20px;
+		}
+
+		.bottom-left {
+			display: flex;
+			flex-direction: column;
+			gap: 12px;
+		}
+
+		.copyright {
+			padding: 30px 0;
+		}
+	}
+	@media (min-width: 1024px) {
+		.footer-container {
+			background-color: #03100e;
+			color: #c4c8cc;
+			display: flex;
+			flex-direction: column;
+		}
+
+		.footer-top {
+			width: 100%;
+			display: flex;
+			justify-content: space-between;
+		}
+
+		.top-left {
+			display: flex;
+			flex-direction: column;
+			gap: 20px;
+		}
+
+		.top-left-title {
+			font-size: 26px;
+			width: 270px;
+		}
+
+		.top-right {
+			display: flex;
+			gap: 80px;
+		}
+
+		.top-right-list {
+			display: flex;
+			flex-direction: column;
+			gap: 20px;
+		}
+
+		.top-right-title {
+			color: #76797e;
+			font-size: 14px;
+		}
+
+		.top-right-content {
+			color: #c5c6c9;
+			font-size: 16px;
+		}
+
+		.footer-bottom {
+			width: 100%;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			padding: 22px 0;
+			margin: 40px 0 22px;
+		}
+
+		.bottom-left {
+			display: flex;
+			flex-direction: column;
+			gap: 12px;
+			width: 270px;
+		}
+
+		.bottom-right {
+			display: flex;
+			gap: 36px;
+		}
+
+		.bottom-left-title {
+			color: #ffffff;
+			font-size: 20px;
+		}
+
+		.bottom-left-content {
+			color: #c4c8cc;
+			font-size: 14px;
+		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,4 @@
 	import Footer from '$lib/components/ui/footer/footer.svelte';
 </script>
 
-<div class="bg-[#03100E] px-[165px] py-[250px]">
-	<Footer />
-</div>
+<Footer />
