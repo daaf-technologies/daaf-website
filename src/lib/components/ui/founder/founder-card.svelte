46ee130c87611fe0ed44a3426c26e0ac4dce6143
roslind-vasantha <roslindvasantha@gmail.com>
2025-08-24 16:22:11 +0530

    feat: :sparkles: Implemented founder card

---
+++ src/lib/components/ui/founder/founder-card.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Button from '$lib/components/design/button/button.svelte';
+
+	interface FounderPropsType {
+		src: any;
+		name: string;
+		designation: string;
+		imgClassname: string;
+	}
+
+	const { src, designation, name, imgClassname }: FounderPropsType = $props();
+</script>
+
+<div class="flex flex-col gap-2 border-[1.6px] border-white bg-white">
+	<img {src} alt="afroze" class={imgClassname} />
+
+	<div class="flex justify-between px-3">
+		<div>
+			<p class="name text-lg font-semibold">{name}</p>
+			<p class="designation text-sm font-semibold text-[#65755F]">{designation}</p>
+		</div>
+
+		<Button variant="primary">Visit</Button>
+	</div>
+</div>
---
+++ src/lib/components/ui/founder/founders.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import FounderCard from './founder-card.svelte';
+	import Afroze from '$lib/assets/images/Afroze.png';
+	import Dharani from '$lib/assets/images/Dharani.png';
+</script>
+
+<div class="flex flex-col gap-2 bg-[#F6F6F6]">
+	<FounderCard
+		src={Dharani}
+		name="Dharani Dharan"
+		designation="Co-Founder & CEO"
+		imgClassname="rotate-[4deg]"
+	/>
+
+	<FounderCard
+		src={Afroze}
+		name="Afroze Ali"
+		designation="Co-Founder & CTO"
+		imgClassname="rotate-[-4deg]"
+	/>
+</div>
