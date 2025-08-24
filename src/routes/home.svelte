88328a571e25ddf5f90692b7648b1f6ca5a3a427
roslind-vasantha <roslindvasantha@gmail.com>
2025-08-24 17:49:21 +0530

    refactor(#32): :recycle: Integrated founders section

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Footer from '$lib/components/ui/footer/footer.svelte';
+	import Founders from '$lib/components/ui/founder/founders.svelte';
 	import Header from '$lib/components/ui/header/header.svelte';
 </script>
 
 <div class="flex w-full flex-col items-center justify-center">
 	<Header />
+	<Founders />
 	<Footer />
 </div>
