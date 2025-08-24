991711377202f9e8ba00a21021507ce778ad9a62
roslind-vasantha <roslindvasantha@gmail.com>
2025-08-24 21:29:01 +0530

    refactor(#32): :recycle: Integrated get in touch section

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -1,11 +1,13 @@
 <script>
 	import Footer from '$lib/components/ui/footer/footer.svelte';
 	import Founders from '$lib/components/ui/founder/founders.svelte';
+	import GetInTouch from '$lib/components/ui/get-in-touch/get-in-touch.svelte';
 	import Header from '$lib/components/ui/header/header.svelte';
 </script>
 
 <div class="flex w-full flex-col items-center justify-center">
 	<Header />
 	<Founders />
+	<GetInTouch />
 	<Footer />
 </div>
