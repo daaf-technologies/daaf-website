144a7f3a091d6f9dff7403ce403ddc557fa555f0
roslind-vasantha <roslindvasantha@gmail.com>
2025-09-22 18:03:40 +0530

    feat(#66): :sparkles: Made the button glow with CSS keyframes

--- src/lib/components/design/button/primary-button.svelte
+++ src/lib/components/design/button/primary-button.svelte
@@ -32,21 +32,75 @@
 
 <style>
 	button {
+		position: relative;
+		z-index: 0;
 		cursor: pointer;
 		color: #fff;
 		padding: 8px 16px;
 		border-radius: 16px;
 		background: #21231e;
 		border: 1.5px solid #21231e;
-		box-shadow:
-			0 0 0 2.76px #8d8d8d42,
-			inset 0 0 0 2.76px #4b4b4b,
-			inset 0 -2.76px 2.76px 0 #21221f;
+		transition: scale 400ms ease-in-out;
 	}
 
 	button[disabled],
 	button[aria-disabled='true'] {
 		opacity: 0.5;
 		cursor: not-allowed;
+	}
+
+	button::after {
+		content: '';
+		z-index: -1;
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		background-color: #21231e;
+		inset: 0;
+		border-radius: inherit;
+		transition: background 400ms ease-in-out;
+	}
+
+	button::before {
+		content: '';
+		background: linear-gradient(45deg, #21221f, #8d8d8d42, #4b4b4b, #21221f);
+		position: absolute;
+		top: -2px;
+		left: -2px;
+		background-size: 400%;
+		z-index: -1;
+		width: calc(100% + 4px);
+		height: calc(100% + 4px);
+		filter: blur(8px);
+		animation: glowing 20s linear infinite;
+		transition: opacity 400ms ease-in-out;
+		border-radius: inherit;
+		opacity: 0;
+	}
+
+	@keyframes glowing {
+		0% {
+			background-position: 0 0;
+		}
+
+		50% {
+			background-position: 400% 0;
+		}
+
+		100% {
+			background-position: 0 0;
+		}
+	}
+
+	button:hover::after {
+		background: transparent;
+	}
+
+	button:hover::before {
+		opacity: 1;
+	}
+
+	button:hover {
+		scale: 1.05;
 	}
 </style>
--- src/lib/components/design/button/secondary-button.svelte
+++ src/lib/components/design/button/secondary-button.svelte
@@ -32,6 +32,8 @@
 
 <style>
 	button {
+		position: relative;
+		z-index: 0;
 		width: 100%;
 		height: 32px;
 		display: flex;
@@ -45,11 +47,63 @@
 		border-radius: 8px;
 		background: #76f349;
 		border: 2.5px solid #21231e;
+		transition: scale 400ms ease-in-out;
 	}
 
 	button[disabled],
 	button[aria-disabled='true'] {
 		opacity: 0.5;
 		cursor: not-allowed;
+	}
+
+	button::after {
+		content: '';
+		z-index: -1;
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		background-color: #76f349;
+		inset: 0;
+		border-radius: inherit;
+		transition: background 300ms ease-in-out;
+	}
+
+	button::before {
+		content: '';
+		background: linear-gradient(45deg, #008000, #38b000, #70e000, #9ef01a, #ccff33, #008000);
+		position: absolute;
+		top: -2px;
+		left: -2px;
+		background-size: 400%;
+		z-index: -1;
+		width: calc(100% + 4px);
+		height: calc(100% + 4px);
+		filter: blur(8px);
+		animation: glowing 20s linear infinite;
+		transition: opacity 0.3s ease-in-out;
+		border-radius: inherit;
+		opacity: 1;
+	}
+
+	@keyframes glowing {
+		0% {
+			background-position: 0 0;
+		}
+
+		50% {
+			background-position: 400% 0;
+		}
+
+		100% {
+			background-position: 0 0;
+		}
+	}
+
+	button:hover::after {
+		background: transparent;
+	}
+
+	button:hover {
+		scale: 1.08;
 	}
 </style>
