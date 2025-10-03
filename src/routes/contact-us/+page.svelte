ff21a671c472c3c288afcddd28db1b628e38b0db
roslind-vasantha <roslindvasantha@gmail.com>
2025-10-03 13:43:49 +0530

    refactor(#79): :recycle: Refactored contact us page to make it responsive

--- src/routes/contact-us/+page.svelte
+++ src/routes/contact-us/+page.svelte
@@ -63,28 +63,28 @@
 
 <div class="h-full w-full">
 	<div
-		class="wrapper flex h-full max-w-full flex-col items-center overflow-y-auto px-4 py-[50px] backdrop-blur-[620px] lg:gap-[140px] lg:px-[200px]"
+		class="wrapper flex h-full max-w-full flex-col items-center overflow-y-auto px-4 py-[50px] backdrop-blur-[620px] md:px-[60px] lg:gap-[140px] lg:px-[90px]"
 	>
 		<Header className="header w-[343px] lg:w-[646px] !gap-[72px]" />
-		<div class="lg:flex lg:gap-[118px]">
+		<div class="md:flex md:gap-[50px] md:pt-[60px] md:pb-[100px] lg:gap-[40px] xl:gap-[118px]">
 			<div
-				class="description flex flex-col gap-3 pt-[70px] pb-[40px] lg:w-[530px] lg:gap-5 lg:pt-0 lg:pb-0"
+				class="description flex flex-col gap-3 pt-[70px] pb-[40px] md:pt-0 md:pb-0 lg:w-fit lg:gap-5 xl:w-[530px]"
 			>
 				<div
-					class="title text-center text-[30px] leading-[120%] font-semibold text-[#21231e] lg:text-left lg:text-[50px]"
+					class="title text-center text-[30px] leading-[120%] font-semibold text-[#21231e] md:w-[10ch] md:text-left md:leading-[120%] md:tracking-[-2px] lg:text-left lg:text-[40px] xl:text-[50px]"
 				>
-					Whether it’s a small step or a bold idea <span class="special font-normal">-</span> the door’s
-					always open
+					Whether it’s a small step or a bold idea <span class="special font-normal">-</span> the
+					<p class="md:w-[12ch]">door’s always open</p>
 				</div>
 				<div
-					class="text-center text-[16px] leading-[140%] font-medium text-[#51636f] lg:text-left lg:text-[22px]"
+					class="text-center text-[16px] leading-[140%] font-medium text-[#696969] md:w-[26ch] md:text-left md:text-[14px] md:leading-[150%] md:font-normal lg:text-left lg:text-[20px] xl:text-[22px]"
 				>
 					One message is all it takes to set your vision in motion. Let's create the solutions your
 					business needs to thrive today and tomorrow.
 				</div>
 
 				<div
-					class="flex flex-col items-center gap-3 py-4 text-center text-[14px] leading-[140%] text-[#51636f] lg:items-start lg:text-left lg:text-[20px]"
+					class="flex flex-col items-center gap-3 py-4 text-center text-[14px] leading-[140%] text-[#51636f] md:items-start lg:items-start lg:text-left lg:text-[20px]"
 				>
 					<div class="flex items-center gap-1">
 						<Location />
@@ -93,7 +93,7 @@
 						</p>
 					</div>
 
-					<p class="w-[30ch] text-base">
+					<p class="lg-text-base w-[26ch] text-left text-base text-[#696969] md:text-sm">
 						Meydan Grandstand, 6th floor, Meydan Road, Nad Al Sheba, Dubai, U.A.E.
 					</p>
 				</div>
@@ -102,18 +102,19 @@
 			<form
 				onsubmit={sendEmail}
 				aria-busy={sending}
-				class="contact-form flex flex-col gap-5 rounded-xl border border-[#C4C8CC] px-4 py-6 lg:w-[432px] lg:border-none lg:px-0 lg:py-0"
+				class="contact-form flex flex-col gap-5 rounded-xl border border-[#C4C8CC] px-4 py-6 md:w-[350px]! md:border-none md:px-0 md:py-0 lg:w-[432px] lg:border-none lg:px-0 lg:py-0"
 			>
 				<p class="text-sm font-normal text-[#21231e]">Please fill these details For Enquiry</p>
 
 				<div class="flex flex-col gap-5">
-					<div class="flex flex-col gap-5 lg:flex-row lg:gap-4">
+					<div class="flex flex-col gap-5 md:flex-row lg:flex-row lg:gap-4">
 						<Input
 							placeholder="Name*"
 							type="text"
 							name="name"
 							bind:value={name}
 							disabled={sending}
+							className="md:w-[190px]!"
 						/>
 						<Input
 							placeholder="E-mail address*"
@@ -121,9 +122,10 @@
 							name="email"
 							bind:value={email}
 							disabled={sending}
+							className="md:w-[190px]!"
 						/>
 					</div>
-					<div class="relative flex">
+					<div class="relative flex md:w-[400px]">
 						<div class="absolute top-[13px] left-[12px] flex items-center justify-center gap-1">
 							<Dubai />
 							<p class="countrycode">+971 -</p>
@@ -140,7 +142,7 @@
 				</div>
 
 				<p
-					class="flex items-baseline justify-center gap-3 rounded-lg border border-white bg-[#d9ffdc66] p-3 text-sm text-[#69747d]"
+					class="flex items-baseline justify-center gap-3 rounded-lg border border-white bg-[#d9ffdc66] p-3 text-sm text-[#69747d] md:w-[400px]"
 				>
 					<span>
 						<Shield />
@@ -155,10 +157,11 @@
 					name="platform"
 					bind:value={platform}
 					disabled={sending}
+					className="md:w-[400px]!"
 				/>
 
 				<textarea
-					class="h-[135px] w-full resize-none rounded-lg border border-[#d6dee6] bg-white p-3 outline-none placeholder:text-sm placeholder:text-[#51636f] placeholder:opacity-100"
+					class="h-[135px] w-full resize-none rounded-lg border border-[#d6dee6] bg-white p-3 outline-none placeholder:text-sm placeholder:text-[#51636f] placeholder:opacity-100 md:w-[400px]"
 					name="query"
 					rows="6"
 					placeholder="Write your query here*"
@@ -202,5 +205,11 @@
 
 	.location {
 		font-family: 'Departure Mono';
+	}
+
+	@media (max-width: 360px) {
+		.wrapper {
+			padding: 8px;
+		}
 	}
 </style>
