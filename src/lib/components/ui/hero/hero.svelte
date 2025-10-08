9617b1018428c05ddf3156fcba157caa25762e4b
roslind-vasantha <roslindvasantha@gmail.com>
2025-10-08 16:04:40 +0530

    refactor(#77): :recycle: Refactored mobile view

--- src/lib/components/ui/hero/hero.svelte
+++ src/lib/components/ui/hero/hero.svelte
@@ -1,15 +1,17 @@
 <script>
-	import Button from '$lib/components/design/button';
 	import Header from '../header';
+	import Button from '$lib/components/design/button';
+	import Globe from '$lib/assets/images/globe.svg';
+	import Mascot from '$lib/assets/images/mascot.svg';
 </script>
 
-<div class="hero flex w-full flex-col items-center pt-[50px]">
+<div class="hero relative flex w-full flex-col items-center pt-[50px]">
 	<Header />
 
 	<div class="flex flex-col items-center justify-center py-[70px] lg:pt-[120px] lg:pb-0">
 		<div class="flex flex-col items-center gap-3 pb-[30px]">
 			<p
-				class="title w-[15ch] text-center text-[30px] leading-[120%] font-semibold text-[#21231E] lg:text-[64px]"
+				class="title w-[15ch] text-center font-[stacion] text-[30px] leading-[120%] font-semibold text-[#21231E] lg:text-[64px]"
 			>
 				Building Fearless Tech for Fearless Brands
 			</p>
@@ -29,32 +31,65 @@
 			<p class="text-base text-nowrap lg:text-[22px]">Explore our services</p>
 		</Button>
 	</div>
+
+	<img src={Globe} class="globe" alt="globe" />
+	<img src={Mascot} class="mascot" alt="mascot" />
 </div>
 
 <style>
 	.hero {
-		background: linear-gradient(to bottom, #ffffff 0%, #d6f5d6 100%);
+		overflow: hidden;
 	}
-
-	.title {
-		font-family: 'Recoleta Alt';
-	}
-
 	@media (max-width: 768px) {
 		.hero {
 			height: 830px;
-			background-image: url('/src/lib/assets/images/home-hero-mobile.png');
+			background-image: url('/src/lib/assets/images/home-hero-bg.svg');
 			background-size: cover;
-			background-position: center;
+			background-repeat: no-repeat;
+		}
+
+		.globe {
+			position: absolute;
+			bottom: -256px;
+			object-fit: cover;
+			width: 546px;
+			height: 546px;
+		}
+
+		.mascot {
+			position: absolute;
+			bottom: 226px;
+			right: 62px;
+			width: 97px;
+			height: 130px;
 		}
 	}
 
-	@media (min-width: 1024px) {
+	@media (min-width: 820px) {
 		.hero {
-			height: 1155px;
-			background-image: url('/src/lib/assets/images/home-hero-desktop.png');
-			background-size: cover;
-			background-position: center;
+			min-height: 1155px;
+			background-image:
+				url('/src/lib/assets/images/home-hero-bg-bottom.svg'),
+				url('/src/lib/assets/images/home-hero-bg.svg');
+			background-size: contain, cover;
+			background-position: bottom, center;
+			background-repeat: no-repeat;
+		}
+
+		.globe {
+			position: absolute;
+			bottom: -570px;
+			object-fit: cover;
+			width: 1106px;
+			height: 1106px;
+		}
+
+		.mascot {
+			position: absolute;
+			bottom: 375px;
+			right: 62px;
+			width: 175px;
+			height: 234px;
 		}
 	}
 </style>
