c454f92c5927ec6f870f657c371e97fa2f1f7bac
roslind-vasantha <roslindvasantha@gmail.com>
2025-10-11 19:41:38 +0530

    refactor(#85): :recycle: Refactored to make it responsive to all screens

--- src/lib/components/ui/founder/founders.svelte
+++ src/lib/components/ui/founder/founders.svelte
@@ -15,12 +15,14 @@
 	<p
 		class="description w-[20ch] text-center text-[24px] font-medium sm:max-w-[25ch] tablet:w-[30ch] tablet:text-[36px] lg:text-[40px]"
 	>
-		Speak directly to the people who <span class="font-light"> plan and build </span> your solutions
+		Speak directly to the people who <span class="font-[stacion] font-light italic">
+			plan and build
+		</span> your solutions
 	</p>
 
-	<div class="mt-[40px] mb-[60px] flex flex-col gap-[50px] tablet:relative">
+	<div class="mt-[40px] mb-[30px] flex flex-col gap-[50px] tablet:relative">
 		<div
-			class="foundercard h-[350px] w-[300px] tablet:absolute lg:top-[150px] lg:right-0 lg:h-[445px] lg:w-[360px]"
+			class="foundercard h-[353px] w-[292px] tablet:absolute tablet:top-[120px] tablet:right-0 tablet:h-[445px] tablet:w-[360px] lg:top-[150px] lg:right-0 lg:h-[445px] lg:w-[360px]"
 		>
 			<FounderCard
 				href="https://www.linkedin.com/in/dharani-dharan-pm/"
@@ -33,7 +35,7 @@
 		</div>
 
 		<div
-			class="foundercard h-[350px] w-[300px] tablet:absolute lg:top-[40px] lg:left-[-40px] lg:h-[445px] lg:w-[360px]"
+			class="foundercard h-[353px] w-[292px] tablet:absolute tablet:top-[20px] tablet:left-[-40px] tablet:h-[445px] tablet:w-[360px] lg:top-[40px] lg:left-[-40px] lg:h-[445px] lg:w-[360px]"
 		>
 			<FounderCard
 				href="https://www.linkedin.com/in/afrze-ali/"
