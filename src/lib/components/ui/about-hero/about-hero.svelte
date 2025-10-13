beb4f1392f364ad4d2716e1d4ef81e5af33f0561
roslind-vasantha <roslindvasantha@gmail.com>
2025-10-13 18:25:49 +0530

    refactor(#80): :recycle: Refactored about us - hero section to make it responsive

--- src/lib/components/ui/about-hero/about-hero.svelte
+++ src/lib/components/ui/about-hero/about-hero.svelte
@@ -7,7 +7,7 @@
 
 	<div class="flex flex-col items-center gap-3 pt-[70px] pb-[90px]">
 		<p
-			class="title w-[18ch] text-center font-[stacion] text-[30px] leading-[120%] font-semibold tablet:text-[60px] lg:text-[64px] xl:w-[25ch] @min-[960px]:text-[64px]"
+			class="title w-[16ch] text-center font-[stacion] text-[30px] leading-[120%] font-semibold tablet:text-[60px] lg:text-[64px] xl:w-[25ch] @min-[960px]:text-[64px]"
 		>
 			We Build What the Future
 			<br /> Demands
@@ -25,27 +25,87 @@
 </div>
 
 <style>
+	.about-card {
+		background-image:
+			url('/src/lib/assets/images/about-hero-bg-bottom.svg'),
+			url('/src/lib/assets/images/about-hero-bg.svg');
+		background-repeat: no-repeat;
+		background-blend-mode: luminosity;
+	}
 	@media (max-width: 768px) {
 		.about-card {
 			height: 720px;
-			background-image: url('/src/lib/assets/images/about-hero-mobile.png');
-			background-size: cover;
-			background-position: center;
+			background-size:
+				685px 294px,
+				auto;
+			background-position:
+				50% 115%,
+				bottom;
 		}
 	}
 
-	/* @media (min-width: 960px) {
-		.title {
-			font-size: 64px;
+	@media (width: 768px) {
+		.about-card {
+			background-size: contain, cover;
 		}
-	} */
+	}
+
+	@media (min-width: 820px) {
+		.about-card {
+			height: 887px;
+			background-position:
+				50% 215%,
+				bottom;
+		}
+	}
 
 	@media (min-width: 1024px) {
 		.about-card {
 			height: 900px;
-			background-image: url('/src/lib/assets/images/about-hero-desktop.png');
-			background-size: cover;
-			background-position: center;
+			background-position:
+				50% 210%,
+				bottom;
+		}
+	}
+
+	@media (min-width: 1500px) {
+		.about-card {
+			background-size: cover, cover;
+			background-position:
+				50% 450px,
+				bottom;
+		}
+	}
+
+	@media (min-width: 1700px) {
+		.about-card {
+			background-position:
+				50% 480px,
+				bottom;
+		}
+	}
+
+	@media (min-width: 2048px) {
+		.about-card {
+			height: 1000px;
+		}
+	}
+
+	@media (min-width: 2500px) {
+		.about-card {
+			height: 1100px;
+		}
+	}
+
+	@media (min-width: 3072px) {
+		.about-card {
+			height: 1200px;
+		}
+	}
+
+	@media (min-width: 3840px) {
+		.about-card {
+			height: 1400px;
 		}
 	}
 </style>
