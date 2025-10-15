9a44097c8c66fa0d4c640d9fbf55ad717fb7e038
roslind-vasantha <roslindvasantha@gmail.com>
2025-10-15 18:51:57 +0530

    feat(#89): :sparkles: Implemented case studies Ui

---
+++ src/lib/components/ui/case-study/case-study.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import Header from '$lib/components/ui/header';
+
+	import SecondaryCard from '$lib/components/design/card/secondary-card.svelte';
+	import DummyCase from '$lib/assets/images/dummy-case.svg';
+</script>
+
+<div class="wrapper">
+	<Header />
+	<p class="section-title">Case Study</p>
+
+	<p class="section-description">
+		From seamless integration to future-ready platforms, DAAF builds what tomorrow demands — today.
+	</p>
+
+	<!-- Tabs -->
+
+	<SecondaryCard
+		title="DAAF: An all-in-one global tax compliance solution"
+		author="Stephanie Neill"
+		image={DummyCase}
+	/>
+
+	<SecondaryCard
+		title="DAAF: An all-in-one global tax compliance solution"
+		author="Stephanie Neill"
+		image={DummyCase}
+	/>
+
+	<SecondaryCard
+		title="DAAF: An all-in-one global tax compliance solution"
+		author="Stephanie Neill"
+		image={DummyCase}
+	/>
+
+	<SecondaryCard
+		title="DAAF: An all-in-one global tax compliance solution"
+		author="Stephanie Neill"
+		image={DummyCase}
+	/>
+
+	<SecondaryCard
+		title="DAAF: An all-in-one global tax compliance solution"
+		author="Stephanie Neill"
+		image={DummyCase}
+	/>
+
+	<SecondaryCard
+		title="DAAF: An all-in-one global tax compliance solution"
+		author="Stephanie Neill"
+		image={DummyCase}
+	/>
+</div>
--- src/routes/case-study/+page.svelte
+++ src/routes/case-study/+page.svelte
@@ -1,1 +1,11 @@
-<h1>Case Study</h1>
+<script lang="ts">
+	import CaseStudy from '$lib/components/ui/case-study';
+	import Footer from '$lib/components/ui/footer/footer.svelte';
+	import GetInTouch from '$lib/components/ui/get-in-touch/get-in-touch.svelte';
+</script>
+
+<div>
+	<CaseStudy />
+	<GetInTouch />
+	<Footer />
+</div>
